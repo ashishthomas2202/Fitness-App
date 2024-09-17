b549ca5db0ecf9f37a97bf1c14165db413fd6cc8
Ashish Thomas <ashishthomas2202@gmail.com>
2024-09-17 19:27:44 -0400

    fixed bugs

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,6 +1,72 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
-import { auth, db } from "@/lib/firebaseConfig";
+import { auth } from "@/lib/firebaseConfig";
+import db from "@/lib/firebase";
+
+// export async function POST(req) {
+//   try {
+//     const { email, password } = await req.json();
+
+//     // Handle user creation with Firebase Authentication
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+
+//     // Create a blank profile for the new user in Firestore
+//     const profileRef = collection(db, "profile"); // Reference to 'profile' collection
+//     await addDoc(profileRef, {
+//       uid: user.uid, // Store uid as a field within the document
+//       firstName: "",
+//       lastName: "",
+//       phoneNumber: "",
+//       DOB: null, // Set as null to allow updates
+//       gender: "",
+//       height: null,
+//       weight: null,
+//       profilePicture: "",
+//     });
+
+//     // Return the basic user details from Firebase Authentication
+//     return new Response(
+//       JSON.stringify({
+//         success: true,
+//         uid: user.uid,
+//         email: user.email,
+//       }),
+//       {
+//         status: 200,
+//         headers: { "Content-Type": "application/json" },
+//       }
+//     );
+//   } catch (error) {
+//     if (error.code === "auth/email-already-in-use") {
+//       return new Response(
+//         JSON.stringify({
+//           success: false,
+//           message: "User with this email already exists.",
+//         }),
+//         {
+//           status: 400,
+//           headers: { "Content-Type": "application/json" },
+//         }
+//       );
+//     }
+
+//     return new Response(
+//       JSON.stringify({
+//         success: false,
+//         message: error.message,
+//       }),
+//       {
+//         status: 500,
+//         headers: { "Content-Type": "application/json" },
+//       }
+//     );
+//   }
+// }
 
 export async function POST(req) {
   try {
@@ -11,53 +77,75 @@ export async function POST(req) {
       auth,
       email,
       password
-    );
-    const user = userCredential.user;
+    )
+      .then(async (response) => {
+        const user = response.user;
 
-    // Create a blank profile for the new user in Firestore
-    const profileRef = collection(db, "profile"); // Reference to 'profile' collection
-    await addDoc(profileRef, {
-      uid: user.uid, // Store uid as a field within the document
-      firstName: "",
-      lastName: "",
-      phoneNumber: "",
-      DOB: null, // Set as null to allow updates
-      gender: "",
-      height: null,
-      weight: null,
-      profilePicture: "",
-    });
+        // Create a user profile in Firestore with default empty fields
+        const userProfile = {
+          uid: user.uid, // Store uid as a field within the document
+          firstName: "",
+          lastName: "",
+          phoneNumber: "",
+          DOB: null, // Set as null to allow updates
+          gender: "",
+          height: null,
+          weight: null,
+          profilePicture: "",
+        };
 
-    // Return the basic user details from Firebase Authentication
-    return new Response(
-      JSON.stringify({
-        success: true,
-        uid: user.uid,
-        email: user.email,
-      }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  } catch (error) {
-    if (error.code === "auth/email-already-in-use") {
-      return new Response(
-        JSON.stringify({
-          success: false,
-          message: "User with this email already exists.",
-        }),
-        {
-          status: 400,
-          headers: { "Content-Type": "application/json" },
+        // Store the profile in Firestore
+        // await setDoc(doc(db, "users", user.uid), userProfile);
+
+        await addDoc(collection(db, "profile"), userProfile).catch((error) => {
+          console.error("Error adding document: ", error);
+        });
+
+        // Return the basic user details from Firebase Authentication
+        return new Response(
+          JSON.stringify({
+            success: true,
+            uid: user.uid,
+            email: user.email,
+          }),
+          {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+      })
+      .catch((error) => {
+        if (error.code === "auth/email-already-in-use") {
+          return new Response(
+            JSON.stringify({
+              success: false,
+              message: "User with this email already exists.",
+            }),
+            {
+              status: 400,
+              headers: { "Content-Type": "application/json" },
+            }
+          );
         }
-      );
-    }
 
+        return new Response(
+          JSON.stringify({
+            success: false,
+            message: error.message,
+          }),
+          {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+      });
+
+    return userCredential;
+  } catch (e) {
     return new Response(
       JSON.stringify({
         success: false,
-        message: error.message,
+        message: "An unexpected error occurred.",
       }),
       {
         status: 500,
--- src/app/api/profile/update-user-profile/route.js
+++ src/app/api/profile/update-user-profile/route.js
@@ -1,29 +1,45 @@
-import { db } from "@/lib/firebaseConfig";
-import { collection, query, where, getDocs, updateDoc } from "firebase/firestore";
-import { NextResponse } from 'next/server';
+// import { db } from "@/lib/firebaseConfig";
+import db from "@/lib/firebase";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { user, profileData } = await request.json();
 
   if (!user || !user.uid || !profileData) {
-    return NextResponse.json({ error: 'User UID and profile data are required' }, { status: 400 });
+    return NextResponse.json(
+      { error: "User UID and profile data are required" },
+      { status: 400 }
+    );
   }
 
   try {
     // Query Firestore for the document with the matching uid
-    const profileRef = collection(db, 'profile');
-    const profileQuery = query(profileRef, where('uid', '==', user.uid));
+    const profileRef = collection(db, "profile");
+    const profileQuery = query(profileRef, where("uid", "==", user.uid));
     const querySnapshot = await getDocs(profileQuery);
 
     if (!querySnapshot.empty) {
       const profileDoc = querySnapshot.docs[0].ref;
       await updateDoc(profileDoc, profileData);
-      return NextResponse.json({ message: 'Profile updated successfully' }, { status: 200 });
+      return NextResponse.json(
+        { message: "Profile updated successfully" },
+        { status: 200 }
+      );
     } else {
-      return NextResponse.json({ error: 'Profile not found' }, { status: 404 });
+      return NextResponse.json({ error: "Profile not found" }, { status: 404 });
     }
   } catch (error) {
-    console.error('Error updating profile:', error);
-    return NextResponse.json({ error: 'Failed to update profile' }, { status: 500 });
+    console.error("Error updating profile:", error);
+    return NextResponse.json(
+      { error: "Failed to update profile" },
+      { status: 500 }
+    );
   }
 }
