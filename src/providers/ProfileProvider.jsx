840dcf178052c9737d34bda3eb33852982fa826a
Ashish Thomas <ashishthomas2202@gmail.com>
2024-10-15 11:04:10 -0400

    updated profileProvider

--- src/providers/ProfileProvider.jsx
+++ src/providers/ProfileProvider.jsx
@@ -11,15 +11,15 @@ import { useSession } from "next-auth/react";
 export const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const getProfile = async () => {
-    if (!session?.user) {
-      // Avoid fetching profile if session is not available
-      return profile;
-    }
+    // if (!session?.user) {
+    //   // Avoid fetching profile if session is not available
+    //   return profile;
+    // }
 
     setLoading(true);
 
@@ -50,10 +50,10 @@ export const ProfileProvider = ({ children }) => {
   //   }
   // }, [session]);
 
-  useLayoutEffect(() => {}, [session]);
+  // useLayoutEffect(() => {}, [session]);
 
   useEffect(() => {
-    if (session?.user) {
+    if (status === "authenticated") {
       // Fetch profile only if session is available and profile doesn't exist
       getProfile();
     } else {
