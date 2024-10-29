fc6bfc1a88237d2877f1333c8bcbc745b7a9a8c0
Michael Babatunde <babamo@farmingdale.edu>
2024-10-29 09:37:57 -0400

    finished models andapi routes

--- src/db/models/WaterIntake.js
+++ src/db/models/WaterIntake.js
@@ -1,33 +1,35 @@
 // src/db/models/WaterIntake.js
 const WaterIntakeSchema = new Schema(
-    {
-      userId: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
-      },
-      amount: {
-        type: Number,
-        required: [true, "Water intake amount is required"],
-        min: [0, "Amount cannot be negative"],
-      },
-      unit: {
-        type: String,
-        enum: ['ml', 'oz'],
-        default: 'ml'
-      },
-      date: {
-        type: Date,
-        required: [true, "Date is required"],
-      },
-      note: {
-        type: String,
-        default: "",
-      }
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
     },
-    { timestamps: true }
-  );
-  
-  WaterIntakeSchema.index({ userId: 1, date: 1 });
-  const WaterIntake = mongoose.models.WaterIntake || mongoose.model("WaterIntake", WaterIntakeSchema);
-  export default WaterIntake;
+    amount: {
+      type: Number,
+      required: [true, "Water intake amount is required"],
+      min: [0, "Amount cannot be negative"],
+    },
+    unit: {
+      type: String,
+      enum: ["ml", "oz"],
+      default: "oz",
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
+    },
+    note: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+WaterIntakeSchema.index({ userId: 1, date: 1 });
+const WaterIntake =
+  mongoose.models.WaterIntake ||
+  mongoose.model("WaterIntake", WaterIntakeSchema);
+export default WaterIntake;
