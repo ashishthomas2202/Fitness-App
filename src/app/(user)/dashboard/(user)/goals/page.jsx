1ebc898b045887cac5430a5f77b94229345400a1
Nicholas Hadzigeorgiou <nickhadzi123@gmail.com>
2024-11-12 08:44:01 -0500

    one last push before demo

--- src/app/(user)/dashboard/(user)/goals/page.jsx
+++ src/app/(user)/dashboard/(user)/goals/page.jsx
@@ -9,17 +9,20 @@ import { ProgressRing } from "@/components/ui/ProgressRing";
 import { Card } from "@/components/ui/Card";
 import {
   FaFire,
+  FaUtensils,
   FaMapMarkerAlt,
   FaTint,
   FaWalking,
   FaMountain,
   FaEdit,
 } from "react-icons/fa";
+import { LuPencil } from "react-icons/lu";
 import { motion, AnimatePresence } from "framer-motion";
 
 export default function GoalsPage() {
   const { data: session } = useSession();
-  const [goals, setGoals] = useState([]);
+  const [goals, setGoals] = useState(null); // Change to null to check if goals exist
+  const [isUpdateMode, setIsUpdateMode] = useState(false);
   const [currentCalories, setCurrentCalories] = useState(0);
   const [currentCaloriesBurned, setCurrentCaloriesBurned] = useState(0);
   const [currentSteps, setCurrentSteps] = useState(0);
@@ -44,19 +47,21 @@ export default function GoalsPage() {
       const response = await axios.get("/api/goals", {
         params: { userId: session.user.id },
       });
-      const goalsData = response.data.data || [];
-      setGoals(goalsData);
-      if (goalsData.length > 0) {
+      const goalsData = response.data.data;
+      if (goalsData && goalsData.length > 0) {
         const goal = goalsData[0];
+        setGoals(goal); // Set the goals state with fetched data
+        setIsUpdateMode(true); // Set update mode if goals exist
         setCalorieGoal(goal.calorieGoal);
         setCaloriesBurnedGoal(goal.caloriesBurnedGoal);
         setStepsGoal(goal.stepsGoal);
         setFlightsGoal(goal.flightsClimbedGoal);
         setDistanceGoal(goal.distanceGoal);
         setWaterIntakeGoal(goal.waterIntakeGoal);
+      } else {
+        setIsUpdateMode(false); // Switch to create mode if no goals found
       }
     } catch (error) {
-      // toast.error("Failed to fetch goals.");
       console.error("Failed to fetch goals:", error);
     }
   };
@@ -74,7 +79,6 @@ export default function GoalsPage() {
       setCurrentDistance(activityData.distance || 0);
       setCurrentWaterIntake(activityData.waterIntake || 0);
     } catch (error) {
-      // toast.error("Failed to fetch today's activity data.");
       console.error("Failed to fetch today's activity data:", error);
     }
   };
@@ -86,16 +90,31 @@ export default function GoalsPage() {
     }
 
     try {
-      await axios.post("/api/goals/create", {
-        ...goalData,
-        userId: session.user.id,
-      });
-      toast.success("Goal saved successfully!");
-      fetchGoals();
+      if (isUpdateMode && goals) {
+        // Update goals if they already exist
+        await handleUpdateGoal(goals._id, goalData);
+      } else {
+        // Create goals if none exist
+        await axios.post("/api/goals/create", {
+          ...goalData,
+          userId: session.user.id,
+        });
+        toast.success("Goal saved successfully!");
+        setIsUpdateMode(true); // Switch to update mode after creation
+        fetchGoals();
+      }
     } catch (error) {
-      toast.error(
-        `Error saving goal: ${error.response?.data?.error || error.message}`
-      );
+      toast.error(`Error saving goal: ${error.response?.data?.error || error.message}`);
+    }
+  };
+
+  const handleUpdateGoal = async (goalId, updatedGoalData) => {
+    try {
+      await axios.put(`/api/goals/update/${goalId}`, updatedGoalData);
+      toast.success("Goal updated successfully!");
+      fetchGoals(); // Refresh goals after update
+    } catch (error) {
+      toast.error(`Error updating goal: ${error.response?.data?.error || error.message}`);
     }
   };
 
@@ -114,9 +133,7 @@ export default function GoalsPage() {
       { current: currentWaterIntake, goal: waterIntakeGoal },
     ];
 
-    // Count how many goals are achieved
     const achievedCount = goals.reduce((count, { current, goal }) => {
-      // Ensure goal is defined and greater than zero, and current meets or exceeds goal
       if (goal > 0 && current >= goal) {
         return count + 1;
       }
@@ -139,29 +156,17 @@ export default function GoalsPage() {
     waterIntakeGoal,
   ]);
 
-  // Congratulatory message for progress
   useEffect(() => {
-    if (currentCalories >= calorieGoal)
-      setMotivationalMessage(
-        "🎉 Congratulations! You've met your calorie intake goal!"
-      );
-    else if (currentCalories >= calorieGoal * 0.75)
+    if (calorieGoal > 0 && currentCalories >= calorieGoal) {
+      setMotivationalMessage("🎉 Congratulations! You've met your calorie intake goal!");
+    } else if (calorieGoal > 0 && currentCalories >= calorieGoal * 0.75) {
       setMotivationalMessage("Almost there! Keep going!");
-    else setMotivationalMessage("Let's keep going towards your goals!");
-  }, [currentCalories]);
+    } else {
+      setMotivationalMessage("Let's keep going towards your goals!");
+    }
+  }, [currentCalories, calorieGoal]);
 
-  // Interactive Goal Card with edit option
-  function GoalCard({
-    title,
-    current,
-    goal,
-    color,
-    unit,
-    icon,
-    message,
-    onSaveGoal,
-    onClearGoal,
-  }) {
+  function GoalCard({ goalId, title, field, current, goal, color, unit, icon, message, onSaveGoal }) {
     const [isEditing, setIsEditing] = useState(false);
     const [newGoal, setNewGoal] = useState(goal);
     const percentage = Math.min((current / (goal || 1)) * 100, 100);
@@ -173,81 +178,32 @@ export default function GoalsPage() {
 
     const handleSave = () => {
       onSaveGoal(newGoal);
+      handleUpdateGoal(goalId, { [field]: newGoal });
       setIsEditing(false);
     };
 
     return (
       <motion.div layout className="w-full">
-        <div
-          className={`goal-card flex flex-col items-center justify-center p-6 bg-white dark:bg-neutral-900 rounded-lg shadow-lg transform transition-transform ${
-            !isEditing ? "hover:scale-105 hover:shadow-xl" : ""
-          }`}
-        >
-          <motion.div
-            layout
-            className="relative flex items-center justify-center mb-4"
-          >
-            {/* Render ProgressRing without percentage text */}
-            <ProgressRing
-              percentage={percentage}
-              color={color}
-              strokeWidth={10}
-              noText={true}
-            />
-
-            {/* Centered container for icon and text */}
+        <div className={`goal-card flex flex-col items-center justify-center p-6 bg-white dark:bg-neutral-900 rounded-lg shadow-lg transform transition-transform ${!isEditing ? "hover:scale-105 hover:shadow-xl" : ""}`}>
+          <motion.div layout className="relative flex items-center justify-center mb-4">
+            <ProgressRing percentage={percentage} color={color} strokeWidth={10} noText={true} />
             <div className="absolute flex flex-col items-center">
-              <div className="text-2xl mb-1">{icon}</div>{" "}
-              {/* Icon positioned above text */}
-              <span className="text-2xl font-bold text-gray-800 dark:text-gray-200">
-                {current} / {goal || 0} {unit}
-              </span>
+              <div className="text-2xl mb-1">{icon}</div>
+              <span className="text-2xl font-bold text-gray-800 dark:text-gray-200">{current} / {goal || 0} {unit}</span>
             </div>
           </motion.div>
-
-          <div className="text-center text-lg font-medium text-gray-600 dark:text-gray-300">
-            {title}
-          </div>
-
-          {message && (
-            <p className="mt-4 text-center text-sm font-semibold text-gray-700 dark:text-gray-300">
-              {message}
-            </p>
-          )}
-
+          <div className="text-center text-lg font-medium text-gray-600 dark:text-gray-300">{title}</div>
+          {message && <p className="mt-4 text-center text-sm font-semibold text-gray-700 dark:text-gray-300">{message}</p>}
           <AnimatePresence>
             {isEditing ? (
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="mt-4 flex space-x-2"
-              >
-                <input
-                  type="number"
-                  value={newGoal}
-                  onChange={(e) => setNewGoal(e.target.value)}
-                  className="border rounded px-2 py-1"
-                />
-                <button
-                  onClick={handleSave}
-                  className="px-2 py-1 text-sm bg-green-500 text-white rounded"
-                >
-                  Confirm
-                </button>
-                <button
-                  onClick={handleEditToggle}
-                  className="px-2 py-1 text-sm bg-gray-300 rounded"
-                >
-                  Cancel
-                </button>
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="mt-4 flex space-x-2">
+                <input type="number" value={newGoal} onChange={(e) => setNewGoal(parseInt(e.target.value))} className="border rounded px-2 py-1" />
+                <button onClick={handleSave} className="px-2 py-1 text-sm bg-indigo-600 text-white rounded">Confirm</button>
+                <button onClick={handleEditToggle} className="px-2 py-1 text-sm bg-gray-300 rounded">Cancel</button>
               </motion.div>
             ) : (
-              <button
-                onClick={handleEditToggle}
-                className="mt-4 px-2 py-1 text-sm bg-blue-500 text-white rounded"
-              >
-                <FaEdit />
+              <button onClick={handleEditToggle} className="absolute bottom-4 right-4 px-2 py-1 text-sm text-blue rounded">
+                <LuPencil />
               </button>
             )}
           </AnimatePresence>
@@ -277,12 +233,8 @@ export default function GoalsPage() {
     }
 
     return (
-      <div
-        className={`mb-6 p-6 ${achievementColor} rounded-lg shadow-lg text-center`}
-      >
-        <h2 className="text-2xl font-bold mb-4 text-neutral-800">
-          Today’s Summary
-        </h2>
+      <div className={`mb-6 p-6 ${achievementColor} rounded-lg shadow-lg text-center`}>
+        <h2 className="text-2xl font-bold mb-4 text-neutral-800">Today’s Summary</h2>
         <div className="flex justify-center items-center space-x-2 mb-4">
           <span className="text-3xl">{emoji}</span>
           <p className="text-xl font-semibold">
@@ -298,88 +250,82 @@ export default function GoalsPage() {
     <div className="p-4">
       <TodaysSummary />
       <h1 className="text-3xl font-bold mb-6">Your Goals</h1>
-      <GoalsForm onSave={(goalData) => handleSaveGoal(goalData)} />
-
-      {/* Metabolic Goals */}
+      <GoalsForm onSave={(goalData) => handleSaveGoal(goalData)} currentGoals={goals} isUpdateMode={isUpdateMode} />
       <div className="mt-8">
         <h2 className="text-xl font-semibold mb-4">Metabolic Goals</h2>
         <Card className="p-6 bg-gray-100 dark:bg-neutral-800 rounded-lg">
           <div className="flex flex-wrap lg:flex-nowrap gap-4 justify-around">
             <GoalCard
               title="Calorie Intake"
+              field="calorieGoal"
+              goalId={goals?._id}
               current={currentCalories}
               goal={calorieGoal}
               color="#4CAF50"
               unit="kcal"
-              icon={<FaFire />}
+              icon={<FaUtensils />}
               message={motivationalMessage}
-              onSaveGoal={setCalorieGoal}
-              onClearGoal={() => setCalorieGoal(null)}
-            />
+              onSaveGoal={setCalorieGoal} />
             <GoalCard
               title="Calories Burned"
+              field="caloriesBurnedGoal"
+              goalId={goals?._id}
               current={currentCaloriesBurned}
               goal={caloriesBurnedGoal}
               color="#FF7043"
               unit="kcal"
               icon={<FaFire />}
-              onSaveGoal={setCaloriesBurnedGoal}
-              onClearGoal={() => setCaloriesBurnedGoal(null)}
-            />
+              onSaveGoal={setCaloriesBurnedGoal} />
             <GoalCard
               title="Water Consumed"
+              field="waterIntakeGoal"
+              goalId={goals?._id}
               current={currentWaterIntake}
               goal={waterIntakeGoal}
               color="#03A9F4"
               unit="L"
               icon={<FaTint />}
-              onSaveGoal={setWaterIntakeGoal}
-              onClearGoal={() => setWaterIntakeGoal(null)}
-            />
+              onSaveGoal={setWaterIntakeGoal} />
           </div>
         </Card>
       </div>
-
-      {/* Activity Goals */}
       <div className="mt-8">
         <h2 className="text-xl font-semibold mb-4">Activity Goals</h2>
         <Card className="p-6 bg-gray-100 dark:bg-neutral-800 rounded-lg">
           <div className="flex flex-wrap lg:flex-nowrap gap-4 justify-around">
             <GoalCard
               title="Steps"
+              field="stepsGoal"
+              goalId={goals?._id}
               current={currentSteps}
               goal={stepsGoal}
               color="#FF9800"
               unit="steps"
               icon={<FaWalking />}
-              onSaveGoal={setStepsGoal}
-              onClearGoal={() => setStepsGoal(null)}
-            />
+              onSaveGoal={setStepsGoal} />
             <GoalCard
               title="Flights Climbed"
+              field="flightsClimbedGoal"
+              goalId={goals?._id}
               current={currentFlights}
               goal={flightsGoal}
               color="#2196F3"
               unit="flights"
               icon={<FaMountain />}
-              onSaveGoal={setFlightsGoal}
-              onClearGoal={() => setFlightsGoal(null)}
-            />
+              onSaveGoal={setFlightsGoal} />
             <GoalCard
               title="Distance"
+              field="distanceGoal"
+              goalId={goals?._id}
               current={currentDistance}
               goal={distanceGoal}
               color="#673AB7"
               unit="km"
               icon={<FaMapMarkerAlt />}
-              onSaveGoal={setDistanceGoal}
-              onClearGoal={() => setDistanceGoal(null)}
-            />
+              onSaveGoal={setDistanceGoal} />
           </div>
         </Card>
       </div>
-
-      {/* Weight Tracker */}
       <div className="mt-8">
         <WeightTracker />
       </div>
--- src/app/api/goals/update/[id]/route.js
+++ src/app/api/goals/update/[id]/route.js
@@ -12,21 +12,36 @@ export async function PUT(req, { params }) {
       return new Response(JSON.stringify({ success: false, message: "Unauthorized User" }), { status: 401 });
     }
 
-    const { calorieGoal, weightGoal, startDate, endDate } = await req.json();
+    const {
+      calorieGoal,
+      weightGoal,
+      stepsGoal,
+      flightsClimbedGoal,
+      distanceGoal,
+      waterIntakeGoal,
+      caloriesBurnedGoal,
+    } = await req.json();
+
     const goal = await Goal.findById(params.id);
 
     if (!goal || goal.userId.toString() !== currentUser.id.toString()) {
       return new Response(JSON.stringify({ success: false, message: "Goal not found or unauthorized" }), { status: 404 });
     }
 
-    goal.calorieGoal = calorieGoal;
-    goal.weightGoal = weightGoal;
-    goal.startDate = new Date(startDate);
-    goal.endDate = new Date(endDate);
+    // Update goal fields with new values
+    goal.calorieGoal = calorieGoal || goal.calorieGoal;
+    goal.weightGoal = weightGoal || goal.weightGoal;
+    goal.stepsGoal = stepsGoal || goal.stepsGoal;
+    goal.flightsClimbedGoal = flightsClimbedGoal || goal.flightsClimbedGoal;
+    goal.distanceGoal = distanceGoal || goal.distanceGoal;
+    goal.waterIntakeGoal = waterIntakeGoal || goal.waterIntakeGoal;
+    goal.caloriesBurnedGoal = caloriesBurnedGoal || goal.caloriesBurnedGoal;
 
     await goal.save();
+
     return new Response(JSON.stringify({ success: true, goal }), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ success: false, message: "Failed to update goal", error: error.message }), { status: 500 });
   }
 }
+
