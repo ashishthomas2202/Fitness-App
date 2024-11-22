19b3355a101ffbd1a00e0c1904129f92ae12943a
Decbelfield <Belfdj@farmingdale.edu>
2024-11-21 20:00:59 -0500

    added a follower count to the trainer dashboard for ease of viewing

--- src/app/(user)/dashboard/trainer/layout.jsx
+++ src/app/(user)/dashboard/trainer/layout.jsx
@@ -1,16 +1,135 @@
+// "use client";
+// import { Header } from "@/components/dashboard/Header";
+// import { Sidebar } from "@/components/dashboard/Sidebar";
+// import { LuGoal, LuLayoutDashboard } from "react-icons/lu";
+// import { GiKnifeFork } from "react-icons/gi";
+// import { IoMdFitness } from "react-icons/io";
+// import { PiChartLineUpBold } from "react-icons/pi";
+// import { HiUserGroup } from "react-icons/hi2";
+// import { FiSettings } from "react-icons/fi";
+// import { GiNoodles } from "react-icons/gi";
+// import React, { useState, useLayoutEffect } from "react";
+// import { cn } from "@/lib/utils";
+// import { Loader2 } from "lucide-react";
+// import axios from "axios";
+// import { useRouter } from "next/navigation";
+// import { toast } from "react-toastify";
+
+// export default function TrainerDashboardLayout({ children }) {
+//   const [loading, setLoading] = useState(true);
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+//   const router = useRouter();
+
+//   const menu = [
+//     {
+//       title: "Dashboard",
+//       icon: <LuLayoutDashboard size={20} />,
+//       href: "/dashboard/trainer",
+//     },
+
+//     {
+//       title: "Meals",
+//       icon: <GiNoodles size={20} />,
+//       href: "/dashboard/trainer/meals",
+//     },
+//     {
+//       title: "Workouts",
+//       icon: <IoMdFitness size={20} />,
+//       href: "/dashboard/trainer/workouts",
+//     },
+//     // {
+//     //   title: "Goals",
+//     //   icon: <LuGoal size={20} />,
+//     //   href: "/dashboard/goals",
+//     // },
+//     // {
+//     //   title: "Meal Plans",
+//     //   icon: <GiKnifeFork size={20} />,
+//     //   href: "/dashboard/meal-plans",
+//     // },
+//     // {
+//     //   title: "Workout Plans",
+//     //   icon: <IoMdFitness size={20} />,
+//     //   href: "/dashboard/workout-plans",
+//     // },
+//     // {
+//     //   title: "Progress",
+//     //   icon: <PiChartLineUpBold size={20} />,
+//     //   href: "/dashboard/progress",
+//     // },
+//     // {
+//     //   title: "Community",
+//     //   icon: <HiUserGroup size={20} />,
+//     //   href: "/dashboard/community",
+//     // },
+//     // {
+//     //   title: "Settings",
+//     //   icon: <FiSettings size={20} />,
+//     //   href: "/dashboard/settings",
+//     // },
+//   ];
+
+//   //   useEffect(() => {
+//   //     setLoading(false);
+//   //   }, []);
+
+//   const isTrainer = async () => {
+//     const trainer = await axios
+//       .get("/api/user/is-trainer")
+//       .then((response) => {
+//         if (response?.data?.success) {
+//           return true;
+//         }
+//         return null;
+//       })
+//       .catch((error) => {
+//         return null;
+//       });
+
+//     if (!trainer) {
+//       router.push("/dashboard");
+//       toast.error("Unauthorized User");
+//     } else {
+//       setLoading(false);
+//     }
+//     return trainer;
+//   };
+
+//   useLayoutEffect(() => {
+//     isTrainer();
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <div className="h-screen w-screen flex justify-center items-center">
+//         <Loader2 className="w-16 h-16 animate-spin" />
+//       </div>
+//     );
+//   }
+//   return (
+//     <section className="bg-slate-100 dark:bg-neutral-950 min-h-screen">
+//       <Sidebar menu={menu} open={sidebarOpen} />
+//       <article
+//         className={cn(
+//           "pr-2 pl-2 sm:pl-20 transition-all duration-300 ease-in-out",
+//           sidebarOpen && "pl-20 sm:pl-44"
+//         )}
+//       >
+//         <Header handleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+//         <main className="rounded-xl overflow-y-auto">{children}</main>
+//       </article>
+//     </section>
+//   );
+// }
 "use client";
 import { Header } from "@/components/dashboard/Header";
 import { Sidebar } from "@/components/dashboard/Sidebar";
-import { LuGoal, LuLayoutDashboard } from "react-icons/lu";
-import { GiKnifeFork } from "react-icons/gi";
-import { IoMdFitness } from "react-icons/io";
-import { PiChartLineUpBold } from "react-icons/pi";
-import { HiUserGroup } from "react-icons/hi2";
-import { FiSettings } from "react-icons/fi";
+import { LuLayoutDashboard } from "react-icons/lu";
 import { GiNoodles } from "react-icons/gi";
+import { IoMdFitness } from "react-icons/io";
+import { Loader2 } from "lucide-react";
 import React, { useState, useLayoutEffect } from "react";
 import { cn } from "@/lib/utils";
-import { Loader2 } from "lucide-react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
@@ -18,6 +137,8 @@ import { toast } from "react-toastify";
 export default function TrainerDashboardLayout({ children }) {
   const [loading, setLoading] = useState(true);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [totalFollowers, setTotalFollowers] = useState(0);
+
   const router = useRouter();
 
   const menu = [
@@ -26,7 +147,6 @@ export default function TrainerDashboardLayout({ children }) {
       icon: <LuLayoutDashboard size={20} />,
       href: "/dashboard/trainer",
     },
-
     {
       title: "Meals",
       icon: <GiNoodles size={20} />,
@@ -37,66 +157,35 @@ export default function TrainerDashboardLayout({ children }) {
       icon: <IoMdFitness size={20} />,
       href: "/dashboard/trainer/workouts",
     },
-    // {
-    //   title: "Goals",
-    //   icon: <LuGoal size={20} />,
-    //   href: "/dashboard/goals",
-    // },
-    // {
-    //   title: "Meal Plans",
-    //   icon: <GiKnifeFork size={20} />,
-    //   href: "/dashboard/meal-plans",
-    // },
-    // {
-    //   title: "Workout Plans",
-    //   icon: <IoMdFitness size={20} />,
-    //   href: "/dashboard/workout-plans",
-    // },
-    // {
-    //   title: "Progress",
-    //   icon: <PiChartLineUpBold size={20} />,
-    //   href: "/dashboard/progress",
-    // },
-    // {
-    //   title: "Community",
-    //   icon: <HiUserGroup size={20} />,
-    //   href: "/dashboard/community",
-    // },
-    // {
-    //   title: "Settings",
-    //   icon: <FiSettings size={20} />,
-    //   href: "/dashboard/settings",
-    // },
   ];
 
-  //   useEffect(() => {
-  //     setLoading(false);
-  //   }, []);
-
   const isTrainer = async () => {
-    const trainer = await axios
-      .get("/api/user/is-trainer")
-      .then((response) => {
-        if (response?.data?.success) {
-          return true;
-        }
-        return null;
-      })
-      .catch((error) => {
-        return null;
-      });
-
-    if (!trainer) {
+    try {
+      const trainer = await axios.get("/api/user/is-trainer");
+      if (trainer?.data?.success) {
+        setLoading(false);
+      } else {
+        router.push("/dashboard");
+        toast.error("Unauthorized User");
+      }
+    } catch {
       router.push("/dashboard");
       toast.error("Unauthorized User");
-    } else {
-      setLoading(false);
     }
-    return trainer;
+  };
+
+  const fetchFollowersCount = async () => {
+    try {
+      const response = await axios.get("/api/follower/total/followers");
+      setTotalFollowers(response.data.data.followers|| 0);
+    } catch (error) {
+      setTotalFollowers(0);
+    }
   };
 
   useLayoutEffect(() => {
     isTrainer();
+    fetchFollowersCount();
   }, []);
 
   if (loading) {
@@ -106,6 +195,7 @@ export default function TrainerDashboardLayout({ children }) {
       </div>
     );
   }
+
   return (
     <section className="bg-slate-100 dark:bg-neutral-950 min-h-screen">
       <Sidebar menu={menu} open={sidebarOpen} />
@@ -116,6 +206,10 @@ export default function TrainerDashboardLayout({ children }) {
         )}
       >
         <Header handleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+        <div className="p-4 bg-white dark:bg-neutral-800 shadow rounded-lg mb-4">
+          <h2 className="text-xl font-bold">Followers</h2>
+          <p className="text-2xl">{followersCount}</p>
+        </div>
         <main className="rounded-xl overflow-y-auto">{children}</main>
       </article>
     </section>
